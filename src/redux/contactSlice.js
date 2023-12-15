546de3f758363780a96963c347a8d4d153bb2817
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2023-12-15 22:09:59 +0200

    add store

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 export const contactSlice = createSlice({
   name: 'contacts',
@@ -17,4 +17,16 @@ export const contactSlice = createSlice({
 
 export const { addContact, deleteContact } = contactSlice.actions;
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactSlice.reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export default contactSlice.reducer;
